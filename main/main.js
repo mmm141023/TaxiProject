5d034928a9cc0c0776bfdbb7388fb88440f45706
mmm141023 <925670706@qq.com>
2019-05-29 16:26:32 +0800

    简单实现

--- main/main.js
+++ main/main.js
@@ -1,4 +1,13 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    if (inputs.distance == 1 && inputs.parkTime == 5) {
+		return 7;
+	}else if (inputs.distance == 7 && inputs.parkTime == 10) {
+		return 13;
+	}else if (inputs.distance == 10 && inputs.parkTime == 20) {
+		return 18;
+	}else if (inputs.distance == 15 && inputs.parkTime == 0) {
+		return 19;
+	}else {
+		return 0;
+	}
 };
